4c735d50be4791d6f19ddbced1e4ca60f9cc5a8c
Adam McGurk <eldermcgurk@gmail.com>
2022-05-23 11:03:23 -0600

    added in a dry run http handler

--- app/Http/AppHttp.ts
+++ app/Http/AppHttp.ts
@@ -1,32 +1,39 @@
 const fetch = require("node-fetch");
+// @ts-ignore I hate hate hate ignoring this...but it just isn't an issue
 class AppHttp {
     private readonly appDomain:string;
-    private headers:Headers;
+    private fetchOptions;
     constructor() {
         this.appDomain = process.env.APP_URL;
         const appToken = process.env.APP_TOKEN;
-        this.headers = new Headers({
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Auth-Token": appToken,
-        });
-    }
-    async get(path:string) {
-        const req = await fetch(this.appDomain + path, {
-            method: "GET",
-            headers: this.headers,
-        });
-        return await AppHttp.handleResponse(req);
-    }
-    async post(path:string, data:string) {
-        const req = await fetch(this.appDomain + path, {
+        this.fetchOptions = {
             method: "POST",
-            headers: this.headers,
-            body: data,
-        });
-        return await AppHttp.handleResponse(req);
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Auth-Token": appToken,
+            }
+        };
     }
-    private static async handleResponse(fetchRequest:Request) {
+    async get(path:string, isDryRun = true) {
+        this.fetchOptions.method = "GET";
+        if (isDryRun) {
+            console.log(`Would GET ${this.appDomain + path}`);
+        } else {
+            const req = await fetch(this.appDomain + path, this.fetchOptions);
+            return await AppHttp.handleResponse(req);
+        }
+    }
+    async post(path:string, data:string, isDryRun = true) {
+        this.fetchOptions.body = data;
+        if (isDryRun) {
+            console.log(`Would POST to ${this.appDomain + path} with body of ${data}`);
+        } else {
+            const req = await fetch(this.appDomain + path, this.fetchOptions);
+            return await AppHttp.handleResponse(req);
+        }
+    }
+    private static async handleResponse(fetchRequest) {
         if (!fetchRequest.ok) {
             const output = await fetchRequest.text();
             console.log(`Fetch request failed with code ${fetchRequest.status} and text ${output}`);
@@ -35,3 +42,4 @@ class AppHttp {
         }
     }
 }
+module.exports.AppHttp = AppHttp;
