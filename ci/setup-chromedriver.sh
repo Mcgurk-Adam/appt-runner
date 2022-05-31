d102dc0b15a7a9bb8887bcdd7729aa01247a3dfc
Adam McGurk <amcgurk@bamboohr.com>
2022-05-31 11:11:28 -0600

    just throwing caution to the wind now

--- ci/setup-chromedriver.sh
+++ ci/setup-chromedriver.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 DRIVER="${CHROMEWEBDRIVER}"
-if [ -f "$DRIVER" ]; then
-  sudo chmod 755 "$DRIVER"
-else
-  echo "Chrome driver file doesn't exist. Value retrieved was $DRIVER"
-  exit 1
-fi
+sudo chmod 755 "$DRIVER"
